ea2bb934eed382c72ff813fe542e3a8c45fd8f73
Dave Baker <dave@dsb3.com>
2018-02-17 14:53:17 -0500

    Split files into functions

---
+++ graph.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+#
+# Generate Horst training pyramids
+#
+
+import sys
+from plot.text import pyramid as text_pyramid
+
+
+# Handle input file as first arg, or default to "./ticks.csv"
+# TODO: make sure file exists
+infile="./ticks.csv"
+
+if len(sys.argv) > 1:
+  infile=sys.argv[1]
+
+
+# Print for RP and "better", or xx and better
+# TODO: validate value
+types="RP"
+if len(sys.argv) > 2:
+  types=sys.argv[2]
+
+text_pyramid(file = infile, show = types)
+
+
