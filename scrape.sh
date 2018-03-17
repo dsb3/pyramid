930f5524d8ce7740a8b369710c0e3f55885702d8
Dave Baker <dave@dsb3.com>
2018-03-16 21:01:30 -0400

    Scrape data

--- scrape.sh
+++ scrape.sh
@@ -4,5 +4,6 @@
 # refresh data
 #
 
-wget -O dave-rope.csv 'https://docs.google.com/spreadsheet/ccc?key=1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg&output=csv'
+wget -4 -O dave-rope.csv 'https://docs.google.com/spreadsheet/ccc?key=1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg&output=csv'
+
 
