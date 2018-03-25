608864961c9288543df572398df3e660b0911dd7
Dave Baker <dave@dsb3.com>
2018-03-25 15:51:29 -0400

    New data; new scrape

--- scrape.sh
+++ scrape.sh
@@ -3,7 +3,41 @@
 # Hardcoded command to scrape from "public link" and
 # refresh data
 #
+# NOTES:
+# - wget -4 skips ipv6 delay/timeouts
+# - Alternate method is to use this URL format:
+#     wget -4 -O dave.csv 'https://docs.google.com/spreadsheet/ccc?key=1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg&output=csv'
+#
 
-wget -4 -O dave.csv 'https://docs.google.com/spreadsheet/ccc?key=1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg&output=csv'
+# Take the regular URL to edit:
+# https://docs.google.com/spreadsheets/d/1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg/edit#gid=0
+#
+# Replace /edit#gid=num with /export?format=XXX&gid=num
+#
+
+BASEURL="https://docs.google.com/spreadsheets/d/"
+SUFFIX="/export?format=csv&gid="
+
+case "$1" in
+
+  cole)
+     DOC=15cOnJtF9VmatGTyBjB5W5qz3rJ_yuUgmfLEYDJvOHjg
+     wget -4 -O cole.csv $BASEURL$DOC${SUFFIX}0
+     ;;
+
+  shane)
+     DOC=1v30fRnnKASoTLeEuGRo0Zg2hcwOSqE95N9B74v4UhAs
+     wget -4 -O shane.csv $BASEURL$DOC${SUFFIX}0
+     ;;
+
+  dave|*)
+     DOC=1MU9vmWep4GDHleJo6Tw0BJb4fo0XjVJuz1p9uYgCrpg
+
+     wget -4 -O dave.csv         $BASEURL$DOC${SUFFIX}0
+     wget -4 -O dave-boulder.csv $BASEURL$DOC${SUFFIX}1825018984
+     wget -4 -O dave-rock.csv    $BASEURL$DOC${SUFFIX}447635240
+     ;;
+
+esac
 
 
